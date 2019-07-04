2fee7105ec139a9bac41318b5c88d4e6cce3746d
Naresh Kumar H <nareshkumarh@live.com>
2019-07-04 08:00:19 -0400

    Updated

---
+++ src/main/java/com/naresh/bankingapp/util/HibernateUtil.java
@@ -0,0 +1,33 @@
+package com.naresh.bankingapp.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	// SessionFactory is an object designed to create sessions that will interact
+	// with our database
+	private static SessionFactory sessionFactory;
+
+	private HibernateUtil() {
+	};
+
+	public static SessionFactory getSessionFactory() {
+
+		// A SessionFactory is set up once for an application!
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure() // configures settings from hibernate.cfg.xml
+				.build();
+		try {
+			sessionFactory = new Configuration().configure().buildSessionFactory();
+		}
+		catch (Exception e) {
+			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+			// so destroy it manually.
+			StandardServiceRegistryBuilder.destroy( registry );
+		}
+		return sessionFactory;
+	}
+
+}
